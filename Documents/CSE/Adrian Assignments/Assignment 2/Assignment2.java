1735f193ed8dda95d055d6f876ccb664f481df81
cvillanueva <cvillan0123@gmail.com>
2016-06-22 18:49:41 -0700

    Initial Commit

---
+++ Documents/CSE/Adrian Assignments/Assignment 2/Assignment2.java
@@ -0,0 +1,37 @@
+package org.Chris;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Assignment2
+ */
+@WebServlet(description = "This is Assignment 2", urlPatterns = { "/Assignment2" })
+public class Assignment2 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("Hello from Console");
+		PrintWriter writer = response.getWriter();
+		writer.println("<h3> Hello from HTML doGet! </h3>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		PrintWriter writer = response.getWriter();
+		writer.println("<h3> Hello from HTML doPost! </h3>");
+	}
+
+}
